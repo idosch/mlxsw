1fbe889307e69a9a49bad570fabbe0c2b1d50257
Matthew Garrett <matthew.garrett@nebula.com>
2013-08-09 17:58:15 -0400

    Add secure_modules() call
    Provide a single call to allow kernel code to determine whether the system
    has been configured to either disable module loading entirely or to load
    only modules signed with a trusted key.
    
    Bugzilla: N/A
    Upstream-status: Fedora mustard.  Replaced by securelevels, but that was nak'd
    
    Signed-off-by: Matthew Garrett <matthew.garrett@nebula.com>

--- include/linux/module.h
+++ include/linux/module.h
@@ -629,6 +629,8 @@ static inline bool module_requested_async_probing(struct module *module)
 	return module && module->async_probe_requested;
 }
 
+extern bool secure_modules(void);
+
 #else /* !CONFIG_MODULES... */
 
 /* Given an address, look for it in the exception tables. */
@@ -745,6 +747,10 @@ static inline bool module_requested_async_probing(struct module *module)
 	return false;
 }
 
+static inline bool secure_modules(void)
+{
+	return false;
+}
 #endif /* CONFIG_MODULES */
 
 #ifdef CONFIG_SYSFS
